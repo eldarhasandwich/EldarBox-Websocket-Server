ace93098df426c2d449bde36b8a6325e1890ae40
Eldar <hasanovicel@gmail.com>
2020-02-02 17:04:25 +1100

    lint

--- test/MasterServer/MasterServer.test.ts
+++ test/MasterServer/MasterServer.test.ts
@@ -9,10 +9,10 @@ const server = CreateServer(8080)
 const masterClient = new WebSocket.w3cwebsocket(`ws://localhost:${port}`, 'echo-protocol')
 
 test('foo', t => {
-	t.pass();
-});
+  t.pass()
+})
 
 test('bar', async t => {
-	const bar = Promise.resolve('bar');
-	t.is(await bar, 'bar');
-});
+  const bar = Promise.resolve('bar')
+  t.is(await bar, 'bar')
+})
